86ed0b480461b78c24d121ec81282511a2ec6aac
anthony-alekseiev <fynjyass@gmail.com>
2022-06-16 21:59:46 +0300

    Initial Commit

---
+++ WordsBoosterTestTask/WordsBoosterTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsBoosterTestTaskApp.swift
+//  WordsBoosterTestTask
+//
+//  Created by Anton Aleksieiev on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsBoosterTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
